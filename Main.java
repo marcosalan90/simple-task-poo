2466378354c5a08596c12b3ac1e31ab1408bea86
marcosalan90 <marcosbraga90@hotmail.com>
2024-08-31 00:28:06 +0000

    feat

--- Main.java
+++ Main.java
@@ -1,71 +1,126 @@
-class Tarefa {
+public class Tarefa {
     private String titulo;
     private String descricao;
     private String prazo;
-    private int prioridade;
+    private String prioridade;
 
-    // Construtor
-    public Tarefa(String titulo, String descricao, String prazo, int prioridade) {
+    public Tarefa(String titulo, String descricao, String prazo, String prioridade) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.prazo = prazo;
         this.prioridade = prioridade;
     }
 
-    // Construtor
-    public Tarefa(String titulo) {
-        this.titulo = titulo;
-        this.descricao = "";
-        this.prazo = "";
-        this.prioridade = 0;
+    public String getTitulo() {
+        return titulo;
     }
 
-    // Métodos get
-    public String getTitulo() {
-        return this.titulo;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     public String getDescricao() {
-        return this.descricao;
-    }
-
-    public String getPrazo() {
-        return this.prazo;
-    }
-
-    public int getPrioridade() {
-        return this.prioridade;
-    }
-
-    // Métodos set
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+        return descricao;
     }
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
+    public String getPrazo() {
+        return prazo;
+    }
+
     public void setPrazo(String prazo) {
         this.prazo = prazo;
     }
 
-    public void setPrioridade(int prioridade) {
-        this.prioridade = prioridade;
+    public String getPrioridade() {
+        return prioridade;
     }
 
-    // Método para obter descrição completa
-    public String getDescricaoCompleta() {
-        return "Título: " + this.titulo + "\n" +
-               "Descrição: " + this.descricao + "\n" +
-               "Prazo: " + this.prazo + "\n" +
-               "Prioridade: " + this.prioridade;
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Tarefa tarefa1 = new Tarefa("Estudos poo", "revisar os conceitos de classes e objetos", "03-09-2024", 1);
-        System.out.println(tarefa1.getDescricaoCompleta());
+        Tarefa tarefa1 = new Tarefa("Estudar Java", "Completar o curso de Java", "2024-09-30", "Alta");
+
+        // Modificar o título
+        tarefa1.setTitulo("Estudar Java Avançado");
+
+        // Acessar o novo título
+        System.out.println(tarefa1.getTitulo());  // Saída: Estudar Java Avançado
+    }
+}
+
+
+public class Tarefa {
+    // ... (outros métodos)
+
+    public String exibirDetalhes() {
+        return "Título: " + titulo + "\nDescrição: " + descricao + "\nPrazo: " + prazo + "\nPrioridade: " + prioridade;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Tarefa tarefa1 = new Tarefa("Estudar Java", "Completar o curso de Java", "2024-09-30", "Alta");
+        System.out.println(tarefa1.exibirDetalhes());
+    }
+}
+
+
+public class Categoria {
+    private String nome;
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
+public class Tarefa {
+    private String titulo;
+    private String descricao;
+    private String prazo;
+    private String prioridade;
+    private Categoria categoria;
+
+    public Tarefa(String titulo, String descricao, String prazo, String prioridade, Categoria categoria) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prazo = prazo;
+        this.prioridade = prioridade;
+        this.categoria = categoria;
+    }
+
+    // ... (outros métodos)
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
+
+// public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Categoria categoria1 = new Categoria("Estudos");
+        Tarefa tarefa1 = new Tarefa("Estudar Java", "Completar o curso de Java", "2024-09-30", "Alta", categoria1);
+
+        System.out.println(tarefa1.getCategoria().getNome());  // Saída: Estudos
     }
 }
